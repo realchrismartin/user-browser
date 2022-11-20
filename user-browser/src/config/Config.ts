f934b3e02c5355291f2f7bc433c90397220e19e3
CM <chris_martin@harvardpilgrim.org>
2022-11-20 17:01:37 -0500

    DEV-27097: Add Dockerfiles and compose.yml for services. Update config to be less verbose

--- user-browser/src/config/Config.ts
+++ user-browser/src/config/Config.ts
@@ -3,7 +3,7 @@ import PermissionConfig from "../types/PermissionConfig";
 //This file contains configuration elements used by the frontend
 
 export const apiConfig = {
-  url: process.env.REACT_APP_SUB_API_URL !== undefined ? process.env.REACT_APP_SUB_API_URL : "http://localhost:8080",
+  url:  process.env.REACT_APP_SUB_API_URL || "http://localhost:8080",
   apiGetUsersRoute: "/api/users",
   apiUpdateUsersRoute: "/api/users",
   apiSignInRoute: "/saml/login",
@@ -13,11 +13,11 @@ export const apiConfig = {
 
 export const permissionConfig : PermissionConfig = {
   write:[
-    process.env.REACT_APP_SUB_WRITE_SG_ID !== undefined ? process.env.REACT_APP_SUB_WRITE_SG_ID : "nonexistent-group-id-write"
+    process.env.REACT_APP_SUB_WRITE_SG_ID || "nonexistent-group-id-write"
   ], //Groups with Write privileges
 
   admin:[
-    process.env.REACT_APP_SUB_ADMIN_SG_ID !== undefined ? process.env.REACT_APP_SUB_ADMIN_SG_ID : "nonexistent-group-id-admin"
+    process.env.REACT_APP_SUB_ADMIN_SG_ID || "nonexistent-group-id-admin"
   ] //Groups with Admin privileges
 }
 
