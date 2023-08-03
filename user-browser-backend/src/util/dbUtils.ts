8650d20b5e7d80e8f1b3cae76ffcc356ba3f7797
CM <chris_martin@harvardpilgrim.org>
2023-08-03 17:21:18 -0400

    DEV-30986: paginate user query

--- user-browser-backend/src/util/dbUtils.ts
+++ user-browser-backend/src/util/dbUtils.ts
@@ -1,5 +1,6 @@
 import { UserBrowserUser } from "../types/UserBrowserUser";
-import { ConnectionPool, IResult } from "mssql";
+import { UserFilter } from "../types/UserFilter";
+import { ConnectionPool, IResult, PreparedStatement, PreparedStatementError } from "mssql";
 
 const config = require("../config/config").default;
 const sql = require("mssql");
@@ -29,24 +30,50 @@ async function createSyntheticData(): Promise<boolean> {
   })
 }
 
-async function getUsers(startIndex:number,count:number): Promise<UserBrowserUser[]> {
-
-  //TODO: respect pagination
-  //Right now this just returns a set number of users
+async function getUsersCount(userFilter:UserFilter): Promise<number> {
+    
+  //TODO: user filter properties to make query
     return new Promise((resolve, reject) => {
       sql
         .connect(config.sql)
         .then((connectionPool: ConnectionPool) => {
           connectionPool
-            .query(config.sql.getUsersQuery)
+            .query(config.sql.getUserCountQuery)
             .then((dbResult: IResult<any>) => {
-                resolve(dbResult.recordset);
+                resolve(parseInt(dbResult.recordset[0][""]));
             });
         })
         .catch((err: Error) => {
           reject(err);
         });
     });
-  }
+}
 
-export { getUsers, createSyntheticData }
+async function getUsers(userFilter:UserFilter,startIndex:number,count:number): Promise<UserBrowserUser[]> {
+
+  //TODO: user filter properties to make query
+
+    return new Promise((resolve, reject) => {
+      sql
+        .connect(config.sql)
+        .then((connectionPool: ConnectionPool) => {
+
+          //Build a prepared statement
+          const statement = new sql.PreparedStatement(connectionPool)
+          statement.input('limit', sql.Int)
+          statement.input('offset', sql.Int)
+
+          statement.prepare(config.sql.getUsersNoFilterQuery).then(()=>{
+            statement.execute({limit: count,offset:startIndex}).then((dbResult:IResult<any>) =>{
+              statement.unprepare().then(()=>{
+                //Resolve the final result.
+                console.log(dbResult.recordset);
+                resolve(dbResult.recordset);
+              }).catch((err : any)=> {console.log(err);});
+            }).catch((err : any)=> {console.log(err);});
+          }).catch((err : any)=> {console.log(err);});
+        }).catch((err : any)=> {console.log(err);});
+  });
+};
+
+export { getUsers, getUsersCount, createSyntheticData }
