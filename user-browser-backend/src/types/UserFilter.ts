6fe9e8e96b9c6f8a6e3982f66f45323184168b5d
CM <chris_martin@harvardpilgrim.org>
2023-08-03 17:20:32 -0400

    DEV-30986: add UserFilter backend type

---
+++ user-browser-backend/src/types/UserFilter.ts
@@ -0,0 +1,17 @@
+type UserFilter = {
+    FirstName: string,
+    LastName: string,
+    Degree: string,
+    Company: string,
+    Title: string,
+    Email: string,
+    Phone: string,
+    FDACenter: string,
+    FDADivision: string,
+    PrincipalInvestigator: string,
+    MainContact: string,
+    NPI1Location: string,
+    HPHCLogin: string
+}
+
+export {UserFilter}
